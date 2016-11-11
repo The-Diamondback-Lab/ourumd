bd1e69b3273612608d166fa8a7db3c6d2195a76e
sidtuli <DBK@Admins-Mac-mini-3.local>
2016-11-10 21:39:04 -0500

    Angular Route Testing

--- angtest/app.js
+++ angtest/app.js
@@ -6,15 +6,32 @@ myApp.controller("MainCtlr",["$scope",function($scope){
 
 myApp.config(function($locationProvider,$stateProvider,$urlRouterProvider){
     //$locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/');
     // Next step is to add in 
     $stateProvider
         .state('home',{
-            url:"/home",
-            templateUrl:"home.html"
+            url:"/",
+            templateUrl:"start.html",
+            controller:"MainCtlr"
         })
-        .state('about',{
-            url:"/about",
-            templateUrl:"home.html"
+        .state('class',{
+            url:"/class/:class",
+            templateUrl:"class.html",
+            controller:"classCtlr"
+            
         });
 });
+
+myApp.controller("classCtlr",["$scope","$http","$stateParams",function($scope,$http,$stateParams){
+    var req = $http({
+        method:"GET",
+        url:"http://127.0.0.1:3000/grades/class/"+$stateParams.class,
+        
+    }).success(function(data){
+        console.log(data);
+    }).error(function(){
+        console.log("Err");
+    });
+    
+}]);
+
